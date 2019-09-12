2e1a27a6b4e549896f9b91d8a762a483e848d967
hugopinto1997 <hugopillo@hotmail.com>
2019-09-12 11:35:26 -0600

    Segundo commit

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ int main(){
     cout<<"Ingrese x: ";
     cin>>n;
 
-    cout<<"El valor del ln(1+"<<n<<") = "<<log(1+n)<<endl;
     cout<<"El valor aprox del ln(1+"<<n<<") = "<<lnatural(n,1);
 
     double R=0;
